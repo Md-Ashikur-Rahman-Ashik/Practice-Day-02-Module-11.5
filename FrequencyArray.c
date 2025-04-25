d8ba7905d53eac9d28ab7079165c6e4a9fc10293
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-25 10:32:45 +0600

    Exploring frequency array

---
+++ FrequencyArray.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int array_size;
+
+    scanf("%d", array_size);
+    int arr[array_size];
+
+    for (int i = 0; i < array_size; i++)
+    {
+        /* code */
+        scanf("%d", &arr[i]);
+    }
+
+    int countFrequency = 0;
+
+    for (int i = 0; i <= 9; i++)
+    {
+        /* code */
+        for (int j = 0; j < array_size; j++)
+        {
+            /* code */
+            if (arr[j])
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+    
+
+    return 0;
+}
